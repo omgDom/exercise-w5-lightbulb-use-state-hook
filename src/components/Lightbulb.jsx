3c7e57df038821eef4846197b49e1aaff7a2280d
omgDom <106763412+omgDom@users.noreply.github.com>
2023-10-24 23:36:49 +0200

    Complete.

--- src/components/Lightbulb.jsx
+++ src/components/Lightbulb.jsx
@@ -1,15 +1,20 @@
+import React, { useState } from 'react';
+
 export const Lightbulb = () => {
   // Initialize the state for on/off
+  const [light, setLight] = useState(false);
 
   const toggleLight = () => {
-    // Toggle the on/off state
-    console.log("Clicked");
+    setLight((prevLight) => !prevLight);
   };
+
   return (
-    <div className="lightbulb-container">
-      <div className="lightbulb" onClick={toggleLight}>
+    <div className={`lightbulb-container ${light ? 'on' : 'off'}`} onClick={toggleLight}>
+      {light ? 'on' : 'off'}
+      <div className={`lightbulb ${light ? 'on' : 'off'}`}>
         <div className="bulb"></div>
       </div>
     </div>
   );
 };
+
